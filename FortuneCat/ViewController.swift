a3eb8c6974ecc808e64f7ae21ca7000e5c48372b
Kouki Saito <dan.addr.skd@gmail.com>
2015-09-16 00:32:04 +0900

    Initial Commit

---
+++ FortuneCat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FortuneCat
+//
+//  Created by Kouki Saito on 2015/09/16.
+//  Copyright © 2015年 FortuneCat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
